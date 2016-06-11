d450287fcbd5774b99e820701bfd5f32d925509c
Fernando M. de Oliveira <fernando.dsfw@gmail.com>
2016-06-11 00:51:36 -0300

    Extending the SpringBootServletInitializer class

--- backend/src/main/java/br/ufpb/dcx/lcc/ia/classificador/Boot.java
+++ backend/src/main/java/br/ufpb/dcx/lcc/ia/classificador/Boot.java
@@ -3,10 +3,12 @@ package br.ufpb.dcx.lcc.ia.classificador;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Boot {
+public class Boot extends SpringBootServletInitializer {
 
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Boot.class);
 	}
