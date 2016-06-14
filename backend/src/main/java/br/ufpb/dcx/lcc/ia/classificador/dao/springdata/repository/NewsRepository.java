6ff9d72d62d82c29a9f18ff489ff092548a44bde
Fernando M. de Oliveira <fernando.dsfw@gmail.com>
2016-06-13 23:39:06 -0300

    Algorithm based in statistics of words and words or dicitionary of words

--- backend/src/main/java/br/ufpb/dcx/lcc/ia/classificador/dao/springdata/repository/NewsRepository.java
+++ backend/src/main/java/br/ufpb/dcx/lcc/ia/classificador/dao/springdata/repository/NewsRepository.java
@@ -1,5 +1,7 @@
 package br.ufpb.dcx.lcc.ia.classificador.dao.springdata.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -17,4 +19,9 @@ public interface NewsRepository extends CrudRepository<News, Long> {
 	@Query("SELECT count(n) FROM News n WHERE n.numberOfComments >= " + ClassificadorUtils.NUM_COMENTARIOS_NOTICIA_SUCESSO)
 	public long countSuccesfulNews();
 
+	@Query("SELECT n FROM News n WHERE n.numberOfComments >= " + ClassificadorUtils.NUM_COMENTARIOS_NOTICIA_SUCESSO)
+	public List<News> getAllSuccessfulNews();
+	
+	@Query("SELECT n FROM News n WHERE n.numberOfComments < " + ClassificadorUtils.NUM_COMENTARIOS_NOTICIA_SUCESSO)
+	public List<News> getAllUnsuccessfulNews();
 }
--- backend/src/main/java/br/ufpb/dcx/lcc/ia/classificador/service/NewsService.java
+++ backend/src/main/java/br/ufpb/dcx/lcc/ia/classificador/service/NewsService.java
@@ -1,15 +1,15 @@
 package br.ufpb.dcx.lcc.ia.classificador.service;
 
-import java.math.BigDecimal;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.ufpb.dcx.lcc.ia.classificador.dao.springdata.repository.NewsRepository;
 import br.ufpb.dcx.lcc.ia.classificador.domain.News;
+import br.ufpb.dcx.lcc.ia.classificador.utils.ClassificadorUtils;
 
 @Service
 public class NewsService {
@@ -21,77 +21,73 @@ public class NewsService {
 	private int numeroTotalDeNoticiasDeSucesso;
 	private int numeroTotalDeNoticiasDeNaoSucesso;
 
-	public List<News> getAllNews() {
-		Iterator<News> newsIterable = this.newsRepository.findAll().iterator();
-		List<News> newsList = new LinkedList<News>();
-		while (newsIterable.hasNext()) {
-			newsList.add(newsIterable.next());
-		}
-		return newsList;
-	}
+	private Map<String, Integer> successfulWords = new HashMap<String, Integer>();
+	private Map<String, Integer> unsuccessfulWords = new HashMap<String, Integer>();
 
 	public boolean rank(String title) {
+		successfulWords = loadWords(this.newsRepository.getAllSuccessfulNews());
+		unsuccessfulWords = loadWords(this.newsRepository.getAllUnsuccessfulNews());
+
 		numeroTotalDeNoticias = (new Long(newsRepository.count())).intValue();
 		numeroTotalDeNoticiasDeSucesso = (new Long(newsRepository.countSuccesfulNews())).intValue();
 		numeroTotalDeNoticiasDeNaoSucesso = numeroTotalDeNoticias - numeroTotalDeNoticiasDeSucesso;
 
-		double multiplicacaoDasProbabilidadesDasPalavrasEmNoticiasDeSucesso = probabilityOfWords(title,
-				numeroTotalDeNoticiasDeSucesso);
+		double probabilidadeDasPalavrasDeNoticiasDeSucesso = probabilityOfWordsInSuccessfulNews(title, successfulWords);
 		double probabilidadesDeNoticiasDeSucesso = probability(numeroTotalDeNoticiasDeSucesso, numeroTotalDeNoticias);
 
-		double multiplicacaoDasProbabilidadesDasPalavrasEmNoticiasDeNaoSucesso = probabilityOfWords(title,
-				numeroTotalDeNoticiasDeNaoSucesso);
+		double probabilidadeDasPalavrasDeNoticiasDeNaoSucesso = probabilityOfWordsInSuccessfulNews(title,
+				unsuccessfulWords);
 		double probabilidadesDeNoticiasDeNaoSucesso = probability(numeroTotalDeNoticiasDeNaoSucesso,
 				numeroTotalDeNoticias);
 
-		double probBefore = (multiplicacaoDasProbabilidadesDasPalavrasEmNoticiasDeSucesso * probabilidadesDeNoticiasDeSucesso)
-				/ ((multiplicacaoDasProbabilidadesDasPalavrasEmNoticiasDeSucesso * probabilidadesDeNoticiasDeSucesso)
-						+ (multiplicacaoDasProbabilidadesDasPalavrasEmNoticiasDeNaoSucesso
-								* probabilidadesDeNoticiasDeNaoSucesso));
-		
-		double cem = 100.0;
-		double probAfter = probBefore * cem;
-		
-//		boolean retorno = probAfter >= ClassificadorUtils.ACCEPTABLE_PERCENTUAL_TO_A_SUCCESFUL_NEWS;
-		System.out.println("==================================================");
-		System.out.println(">>> Total de notícias cadastradas: " + numeroTotalDeNoticias);
-		System.out.println(">>> Total de notícias de sucesso: " + numeroTotalDeNoticiasDeSucesso);
-		System.out.println(">>> Total de notícias de nao sucesso: " + numeroTotalDeNoticiasDeNaoSucesso);
-		System.out.println("=====");
-		System.out.println(">>> Probab. sucesso: " + probabilidadesDeNoticiasDeSucesso);
-		System.out.println(">>> Probab. não sucesso: " + probabilidadesDeNoticiasDeNaoSucesso);
-		System.out.println("=====");
-		System.out.println(">>> Probabilidade  antes é: " + (new BigDecimal(probBefore).toPlainString()));
-		System.out.println(">>> Probabilidade  depois é: " + 
-				(new BigDecimal(probBefore).multiply(new BigDecimal(100.0)).toPlainString()));
-//		System.out.println(">>> Retorno é: " + retorno);
-		System.out.println("==================================================");
+		double probabilidadeTotal = ((probabilidadeDasPalavrasDeNoticiasDeSucesso * probabilidadesDeNoticiasDeSucesso)
+				/ ((probabilidadeDasPalavrasDeNoticiasDeSucesso * probabilidadesDeNoticiasDeSucesso)
+						+ (probabilidadeDasPalavrasDeNoticiasDeNaoSucesso * probabilidadesDeNoticiasDeNaoSucesso)));
 
-		return false;
+		System.out.println("A probabilidade foi: " + probabilidadeTotal);
+		return probabilidadeTotal >= ClassificadorUtils.ACCEPTABLE_PERCENTUAL_TO_A_SUCCESFUL_NEWS;
 	}
 
-	private double probabilityOfWords(String title, int totalNumber) {
-		double probab = 1.0;
+	private double probabilityOfWordsInSuccessfulNews(String title, Map<String, Integer> wordsMap) {
+		double probability = 1.0;
 
-		String[] words = title.split(" ");
-		for (int i = 0; i < words.length; i++) {
-			int countOfNewsThatHasTheWord = newsRepository.countNewsThatHasWord(words[i]);
-			probab *= probability(countOfNewsThatHasTheWord, totalNumber);
-			
-			System.out.println("\t=====");
-			System.out.println("\t>>> O count de '" + words[i] + "' é: " + countOfNewsThatHasTheWord);
-			System.out.println("\t>>> O total para o calculo da proab é: " + totalNumber);
-			System.out.println("\t>>> Probabilidade de '" + words[i] + "' é: " + probability(countOfNewsThatHasTheWord, totalNumber));
-			System.out.println("\t=====");
+		String[] wordsOfTitle = title.split(" ");
+		for (int i = 0; i < wordsOfTitle.length; i++) {
+			String wordLower = wordsOfTitle[i].toLowerCase();
+			if (wordsMap.containsKey(wordLower)) {
+				int ocorrenciasDaPalavra = wordsMap.get(wordLower);
+				probability *= probability(1, ocorrenciasDaPalavra);
+			} else {
+				probability *= 0;
+			}
 		}
-		System.out.println("==================================================");
-		return probab;
+		return probability;
+	}
+
+	private Map<String, Integer> loadWords(List<News> list) {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+
+		for (News n : list) {
+			String[] words = n.getTitle().split(" ");
+			for (int i = 0; i < words.length; i++) {
+				String word = words[i];
+				if (map.containsKey(word.toLowerCase())) {
+					Integer value = map.remove(word.toLowerCase());
+					map.put(word.toLowerCase(), value++);
+				} else {
+					map.put(word.toLowerCase(), 1);
+				}
+			}
+		}
+
+		return map;
 	}
 
 	private double probability(int partial, int total) {
-		double partialDouble = partial * 1.0;
-		double totalDouble  = total * 1.0;
-		
+		double partialDouble = (double) partial;
+		double totalDouble = (double) total;
+
 		return (partialDouble / totalDouble);
 	}
+
 }
